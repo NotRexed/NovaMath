5ba9dca21cb210c6e644a10df0afa4ab83a20c59
NotRexed <therealnothingfn@gmail.com>
2025-12-30 19:21:49 -0500

    hg

--- fullscreen.js
+++
@@ -1,8 +0,0 @@
-const BUTTON = document.querySelector('button')
-const IFRAME = document.querySelector('.game')
-
-const toggleFullscreen = () => {
-    IFRAME.requestFullscreen()
-}
-
-BUTTON.addEventListener('click', toggleFullscreen)
